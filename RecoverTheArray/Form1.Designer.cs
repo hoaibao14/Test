fa3a205362f95712cc0dd2335674073161c92abc
hoaibao14 <33025873+hoaibao14@users.noreply.github.com>
2017-10-23 19:01:03 +0700

    Initial commit

---
+++ RecoverTheArray/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace RecoverTheArray
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tbPath = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(53, 32);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // tbPath
+            // 
+            this.tbPath.Location = new System.Drawing.Point(53, 6);
+            this.tbPath.Name = "tbPath";
+            this.tbPath.Size = new System.Drawing.Size(281, 20);
+            this.tbPath.TabIndex = 2;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(53, 61);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(345, 144);
+            this.tbResult.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(340, 3);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(58, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Open file";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 217);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbPath);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox tbPath;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ RecoverTheArray/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RecoverTheArray
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oFile = new OpenFileDialog();
+            oFile.Filter = "All files (*.*)|*.*";
+            if (oFile.ShowDialog() == DialogResult.OK)
+            {
+                tbPath.Text = oFile.FileName;
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string path = tbPath.Text;
+                var reader = new StreamReader(File.OpenRead(path));
+                var line = reader.ReadLine();
+                int n = int.Parse(line.ToString());
+                if ((n >= 2) && (n <= 100000))
+                {
+                    var line2 = reader.ReadLine();
+                    string[] mString = line2.Split(' ');
+                    int[] m = Array.ConvertAll(mString, s => int.Parse(s));
+                    int i = m.Count();
+                    if (n == i)
+                    {
+                        int numberOfArray = 0;
+                        string contentArray = "";
+                        while (i != 0)
+                        {
+
+                            int m1 = m[0];
+                            for (int j = 0; j <= m1; j++)
+                            {
+                                contentArray += m[j].ToString() + " ";
+                            }
+                            contentArray += "\r\n";
+
+                            m = m.Skip(m1 + 1).Take(m.Count() - m1 - 1).ToArray();
+                            i = m.Count();
+                            numberOfArray++;
+
+                        }
+                        tbResult.Text = numberOfArray.ToString() + "\r\n" + contentArray;
+                    }
+                    else {
+                        tbResult.Text = "Number of element is not equal number of integers in the file!";
+                    }
+                }
+                else {
+                    tbResult.Text = "Invalid n!";
+                }
+            }
+            catch (Exception ex)
+            {
+                tbResult.Text = "Processing file: error!";
+            }
+        }
+    }
+}
